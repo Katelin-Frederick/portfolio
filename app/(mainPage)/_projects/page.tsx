769763db94ea908842a76a3bc2fe01445662f3ab
Katelin Frederick <k.frederick94@gmail.com>
2026-02-28 16:36:13 -0500

    Add project section

--- app/(mainPage)/_projects/page.tsx
+++ app/(mainPage)/_projects/page.tsx
@@ -1,7 +1,36 @@
+import React from 'react'
+
+import { CarouselPrevious, CarouselContent, CarouselItem, CarouselNext, Carousel, } from '@/components/ui/carousel'
+import ProjectCard from '@/components/ProjectCard'
+
+import projectData from './projectData'
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <section id='projects' className='mb-24 snap-start scroll-my-20 md:scroll-my-28'>
+    <h2 className='text-3xl md:text-5xl text-center m-12'>Projects</h2>
+
+    <div className='max-w-screen px-12'>
+      <Carousel
+        opts={{
+          align: 'start',
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {projectData.map((project, index) => (
+            <CarouselItem key={index} className='flex justify-center xl:basis-1/2 2xl:basis-1/3'>
+              <div className='flex flex-col w-full max-w-[384px] items-center'>
+                <ProjectCard {...project} />
+              </div>
+            </CarouselItem>
+
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  </section>
 )
 
 export default Projects
---
+++ components/ProjectCard.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import Image from 'next/image'
+import React from 'react'
+
+import { cn, } from '@/lib/utils'
+
+import { CardDescription, CardContent, CardHeader, CardAction, CardTitle, Card, } from './ui/card'
+import { Button, } from './ui/button'
+
+type ProjectCardProps = {
+  image: {
+    src: string,
+    alt: string,
+  },
+  title: string,
+  description: string,
+  btnLinks: {
+    demo: string,
+    code: string,
+  },
+  skills: string[],
+}
+
+const ProjectCard = ({
+  image,
+  title,
+  description,
+  btnLinks,
+  skills,
+}: ProjectCardProps) => (
+  <div className='flex flex-col h-full'>
+    <div className={cn('relative w-60 h-60 sm:w-[384px] sm:h-96')}>
+      <Image
+        src={image.src}
+        fill
+        alt={image.alt}
+        className='rounded-t-sm border-b-gold-500 border-b-4 object-cover'
+      />
+    </div>
+
+    <Card className='flex flex-col flex-1 w-60 sm:w-96 p-1.5 rounded-t-none border-t-0'>
+      <CardHeader className='p-0'>
+        <CardTitle className='text-center text-2xl py-3'>{title}</CardTitle>
+        <CardDescription className='text-center'>
+          {description}
+        </CardDescription>
+      </CardHeader>
+
+      <CardAction className='flex justify-around items-center w-full px-1 mt-auto mb-2'>
+        <Button size='sm' className='mr-3.5'>
+          <a href={btnLinks.demo} target='_blank' rel='noopener noreferrer' tabIndex={-1}>
+            View Demo
+          </a>
+        </Button>
+        <Button size='sm'>
+          <a href={btnLinks.code} target='_blank' rel='noopener noreferrer' tabIndex={-1}>
+            View Code
+          </a>
+        </Button>
+      </CardAction>
+
+      <CardContent className='p-0'>
+        <p>Skills Used:</p>
+        <ul className='mt-1.5 list-none'>
+          {skills.map((skill, index) => (
+            <li key={index} className='border border-gray-500 bg-gray-100 p-3'>
+              {skill}
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  </div>
+)
+
+
+export default ProjectCard
